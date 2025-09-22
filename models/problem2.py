600b6d1cb9d64040fbdaba795b37d7df3fe7d1d7
aaronbernal28 <aaronbernal28@gmail.com>
2025-09-22 19:32:08 -0300

    .

--- models/problem2.py
+++ models/problem2.py
@@ -7,30 +7,32 @@ def Routing_naive(block1, block2):
     capacidad, costo_fijo, costo_variable, coord_I, coord_J = block2
     model, x, y = asignacion(I, J, c, S, k, a)
     cost_1 = round(model.getObjVal(), 2)
-    asignaciones = {i: [] for i in range(len(I))}
 
+    asignaciones = {i: [] for i in range(len(I))}
     for j in J:
         for i in I:
             if model.getVal(x[i, j]) == 1:
                 asignaciones[i].append(j)
     
-    for i in I:
+    for i in I + [-1]:
         if len(asignaciones[i]) > 0:
             ''' -1 is the node i temporally'''
-            coords = np.vstack([coord_J[asignaciones[i]], coord_I[i]])
+            data = {}
+            data['indexes'] = asignaciones[i] + [-1]
 
             # define distance function (in this case euclidean)
-            dist_matrix = np.linalg.norm(coords[:, np.newaxis] - coords[np.newaxis, :], axis=-1)
-            def dist(i, j):
-                return int(dist_matrix[i, j])
+            coords = np.vstack([coord_J[asignaciones[i]], coord_I[i]])
+            data['distance_matrix'] = np.linalg.norm(coords[:, np.newaxis] - coords[np.newaxis, :], axis=-1)
             
-            indexes = asignaciones[i] + [-1]
-            output = run_routing(indexes, dist, capacidad, costo_fijo, costo_variable)
+            data['demands'] = [1] * len(data['indexes'])
+            data["vehicle_capacities"] = [capacidad]*len(J)
+            data["num_vehicles"] = len(J) # no esta acotado
+            data["depot"] = -1
+            output = run_routing(data)
     return None
 
-def run_routing(indexes, dist, capacidad, costo_fijo, costo_variable):
+def run_routing(data):
     return None
-
 
 if __name__ == "__main__":
     coords = np.array([[0, 0], [1, 2], [2, 1], [3, 3]])
